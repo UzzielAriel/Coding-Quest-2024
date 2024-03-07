a9ce4a0ebc5cbab67824ee0d2b00469e07c642a0
UzzielAriel <uzzielariel@gmail.com>
2024-03-07 13:00:29 +0000

    Add Day 4

---
+++ Day 4/4.py
@@ -0,0 +1,24 @@
+import math
+
+d = lambda x1, x2, y1, y2, z1, z2: math.sqrt((x1-x2)**2 + (y1-y2)**2  + (z1-z2)**2)
+f = open("4.txt", "r").readlines()[1:]
+
+t = 9999999999999999999
+p = ""
+
+for i in f:
+	for j in f:
+		if i == j:
+			continue
+		s1 = i.split()
+		s2 = j.split()
+		
+		v1 = list(map(float, [x for x in s1[-3:]]))
+		v2 = list(map(float, [x for x in s2[-3:]]))
+
+		cd = d(v1[0], v2[0], v1[1], v2[1], v1[2], v2[2])
+
+		if cd < t:
+			t = cd
+
+print(t)

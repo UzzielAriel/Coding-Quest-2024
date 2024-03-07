329126ead0c06d7e693c520ce8789fca873549ba
UzzielAriel <uzzielariel@gmail.com>
2024-03-07 13:44:09 +0000

    Added visualization

--- Day 4/4.py
+++ Day 4/4.py
@@ -1,4 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import math
+ 
+fig = plt.figure()
+ax = plt.axes(projection='3d')
 
 d = lambda x1, x2, y1, y2, z1, z2: math.sqrt((x1-x2)**2 + (y1-y2)**2  + (z1-z2)**2)
 f = open("4.txt", "r").readlines()[1:]
@@ -15,10 +20,17 @@ for i in f:
 		
 		v1 = list(map(float, [x for x in s1[-3:]]))
 		v2 = list(map(float, [x for x in s2[-3:]]))
-
+		
 		cd = d(v1[0], v2[0], v1[1], v2[1], v1[2], v2[2])
 
 		if cd < t:
 			t = cd
 
 print(t)
+
+for i in f:
+	s1 = i.split()
+	v1 = list(map(float, [x for x in s1[-3:]]))
+	ax.scatter(v1[0], v1[1], v1[2])
+
+plt.show()
